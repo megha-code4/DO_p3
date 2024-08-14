e84c3ec931d4c6b3e1ede62e6273051fa5945c43
megha-code4 <code.megha.j@gmail.com>
2024-08-14 08:48:22 +0530

    commit

---
+++ src/main/java/com/Tc6A/app/App.java
@@ -0,0 +1,13 @@
+package com.Tc6A.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
